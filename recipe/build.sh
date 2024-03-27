d4ebe440478ad5e9bade5e75c77052a437170c27
Timothy Cera <timothy.cera@noaa.gov>
2024-03-27 11:08:57 -0400

    Skip two ctest tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -29,7 +29,7 @@ cmake -G "${CMAKE_GENERATOR}" \
 make
 make install
 
-SKIP=""
+SKIP="-E test_polar_stereo_neighbor_budget_vector_grib1_4|test_polar_stereo_neighbor_budget_vector_grib2_4"
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
   ctest -VV --output-on-failure -j"${CPU_COUNT}" "${SKIP}"
